0c2c7154e58c2a1d8e34eda62a9ff786dd006f7e
zenoix <41812358+zenoix@users.noreply.github.com>
2024-08-31 01:30:43 +1200

    Add zsh functions

--- home-manager/zsh.nix
+++ home-manager/zsh.nix
@@ -298,6 +298,115 @@
         echo master
         return 1
     }
+
+    # mkcd is equivalent to takedir
+    function mkcd takedir() {
+        mkdir -p $@ && cd ''${@:$#}
+    }
+    
+    function takeurl() {
+        local data thedir
+        data="$(mktemp)"
+        curl -L "$1" > "$data"
+        tar xf "$data"
+        thedir="$(tar tf "$data" | head -n 1)"
+        rm "$data"
+        cd "$thedir"
+    }
+    
+    function takegit() {
+        git clone "$1"
+        cd "$(basename ''${1%%.git})"
+    }
+    
+    function take() {
+        if [[ $1 =~ ^(https?|ftp).*\.(tar\.(gz|bz2|xz)|tgz)$ ]]; then
+            takeurl "$1"
+        elif [[ $1 =~ ^([A-Za-z0-9]\+@|https?|git|ssh|ftps?|rsync).*\.git/?$ ]]; then
+            takegit "$1"
+        else
+            takedir "$@"
+        fi
+    }
+
+    function detect-clipboard() {
+        emulate -L zsh
+    
+        if [[ "''${OSTYPE}" == darwin* ]] && (( ''${+commands[pbcopy]} )) && (( ''${+commands[pbpaste]} )); then
+            function clipcopy() { cat "''${1:-/dev/stdin}" | pbcopy; }
+            function clippaste() { pbpaste; }
+        elif [[ "''${OSTYPE}" == (cygwin|msys)* ]]; then
+            function clipcopy() { cat "''${1:-/dev/stdin}" > /dev/clipboard; }
+            function clippaste() { cat /dev/clipboard; }
+        elif (( $+commands[clip.exe] )) && (( $+commands[powershell.exe] )); then
+            function clipcopy() { cat "''${1:-/dev/stdin}" | clip.exe; }
+            function clippaste() { powershell.exe -noprofile -command Get-Clipboard; }
+        elif [ -n "''${WAYLAND_DISPLAY:-}" ] && (( ''${+commands[wl-copy]} )) && (( ''${+commands[wl-paste]} )); then
+            function clipcopy() { cat "''${1:-/dev/stdin}" | wl-copy &>/dev/null &|; }
+            function clippaste() { wl-paste --no-newline; }
+        elif [ -n "''${DISPLAY:-}" ] && (( ''${+commands[xsel]} )); then
+            function clipcopy() { cat "''${1:-/dev/stdin}" | xsel --clipboard --input; }
+            function clippaste() { xsel --clipboard --output; }
+        elif [ -n "''${DISPLAY:-}" ] && (( ''${+commands[xclip]} )); then
+            function clipcopy() { cat "''${1:-/dev/stdin}" | xclip -selection clipboard -in &>/dev/null &|; }
+            function clippaste() { xclip -out -selection clipboard; }
+        elif (( ''${+commands[lemonade]} )); then
+            function clipcopy() { cat "''${1:-/dev/stdin}" | lemonade copy; }
+            function clippaste() { lemonade paste; }
+        elif (( ''${+commands[doitclient]} )); then
+            function clipcopy() { cat "''${1:-/dev/stdin}" | doitclient wclip; }
+            function clippaste() { doitclient wclip -r; }
+        elif (( ''${+commands[win32yank]} )); then
+            function clipcopy() { cat "''${1:-/dev/stdin}" | win32yank -i; }
+            function clippaste() { win32yank -o; }
+        elif [[ $OSTYPE == linux-android* ]] && (( $+commands[termux-clipboard-set] )); then
+            function clipcopy() { cat "''${1:-/dev/stdin}" | termux-clipboard-set; }
+            function clippaste() { termux-clipboard-get; }
+        elif [ -n "''${TMUX:-}" ] && (( ''${+commands[tmux]} )); then
+            function clipcopy() { tmux load-buffer "''${1:--}"; }
+            function clippaste() { tmux save-buffer -; }
+        else
+            function _retry_clipboard_detection_or_fail() {
+                local clipcmd="''${1}"; shift
+                if detect-clipboard; then
+                    "''${clipcmd}" "$@"
+                else
+                    print "''${clipcmd}: Platform $OSTYPE not supported or xclip/xsel not installed" >&2
+                    return 1
+                fi
+            }
+            function clipcopy() { _retry_clipboard_detection_or_fail clipcopy "$@"; }
+            function clippaste() { _retry_clipboard_detection_or_fail clippaste "$@"; }
+            return 1
+        fi
+    }
+    
+    function clipcopy clippaste {
+        unfunction clipcopy clippaste
+        detect-clipboard || true # let one retry
+        "$0" "$@"
+    }
+    
+    
+    function copypath {
+        # If no argument passed, use current directory
+        local file="''${1:-.}"
+    
+        # If argument is not an absolute path, prepend $PWD
+        [[ $file = /* ]] || file="$PWD/$file"
+    
+        # Copy the absolute path without resolving symlinks
+        # If clipcopy fails, exit the function with an error
+        print -n "''${file:a}" | clipcopy || return 1
+    
+        echo ''${(%):-"%B''${file:a}%b copied to clipboard."}
+    }
+    
+    
+    function copyfile {
+        emulate -L zsh
+        clipcopy $1
+    }
     '';
   };
 }
