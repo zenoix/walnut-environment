4aa4fc648462ba8bf4a0c4e7fcd38dadc3ba21dc
zenoix <41812358+zenoix@users.noreply.github.com>
2024-09-02 21:24:21 +1200

    Fix wallpapers and format stylix.nix

--- themes/stylix.nix
+++ themes/stylix.nix
@@ -1,15 +1,17 @@
 { pkgs, ... }: {
-  stylix ={
+  stylix = {
     enable = true;
     polarity = "dark";
-    image = ../assets/wallpaper.jpeg;
 
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+
+    # Stylix needs an image for some reason
+    image = ../assets/fuji-palette.png;
 
     fonts = {
       monospace = {
         package = pkgs.jetbrains-mono;
-	name = "JetBrainsMono";
+        name = "JetBrainsMono";
       };
     };
 
