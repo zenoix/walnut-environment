4f8e73f8c2f0605e4bb6972b6d19bb4d6ba28bfa
zenoix <41812358+zenoix@users.noreply.github.com>
2024-09-07 15:42:51 +1200

    Add latex rendering to render-markdown.nix

--- home-manager/modules/nixvim/plugins/render-markdown.nix
+++ home-manager/modules/nixvim/plugins/render-markdown.nix
@@ -1,14 +1,18 @@
 { pkgs, ... }:
 {
-  programs.nixvim.extraPlugins = [
-    (pkgs.vimUtils.buildVimPlugin {
-      name = "render-markdown";
-      src = pkgs.fetchFromGitHub {
-        owner = "MeanderingProgrammer";
-        repo = "render-markdown.nvim";
-        rev = "2f9d4f0be8784ed4fef5960eb7b80bf60c5fdf56";
-        hash = "sha256-VCGAkcUIynRTErcGlaMWd+uo2KN1f3suPpGEimAhWHM=";
-      };
-    })
-  ];
+  programs.nixvim = {
+    extraPlugins = [
+      (pkgs.vimUtils.buildVimPlugin {
+        name = "render-markdown";
+        src = pkgs.fetchFromGitHub {
+          owner = "MeanderingProgrammer";
+          repo = "render-markdown.nvim";
+          rev = "2f9d4f0be8784ed4fef5960eb7b80bf60c5fdf56";
+          hash = "sha256-VCGAkcUIynRTErcGlaMWd+uo2KN1f3suPpGEimAhWHM=";
+        };
+      })
+    ];
+
+    extraPackages = [ pkgs.python312Packages.pylatexenc ];
+  };
 }
