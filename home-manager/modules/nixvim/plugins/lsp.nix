c968e08d0355cc0900a3390655fe215a657122ce
zenoix <41812358+zenoix@users.noreply.github.com>
2024-09-08 22:09:57 +1200

    Add lsp_signature

--- home-manager/modules/nixvim/plugins/lsp.nix
+++ home-manager/modules/nixvim/plugins/lsp.nix
@@ -1,5 +1,16 @@
 { pkgs-unstable, ... }:
 {
+  programs.nixvim.extraPlugins = with pkgs-unstable.vimPlugins; [ lsp_signature-nvim ];
+  programs.nixvim.extraConfigLua = ''
+    require('lsp_signature').setup({
+      bind = true,
+      hint_prefix = " ",
+      handler_opts = {
+        border = "rounded",
+      },
+      timer_interval = 100,
+    })
+  '';
   programs.nixvim.plugins = {
     lsp-format.enable = true;
 
@@ -20,6 +31,8 @@
           "<leader>rn" = "rename";
         };
       };
+
+      onAttach = "require('lsp_signature').on_attach(signature_setup, bufnr)";
 
       postConfig = ''
         local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
