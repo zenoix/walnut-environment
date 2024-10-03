22349962cbed8108c4736a53e438261f4bff7324
zenoix <41812358+zenoix@users.noreply.github.com>
2024-10-03 19:21:54 +1300

    chore(fastfetch): change fastfetch logo

--- home-manager/modules/fastfetch.nix
+++ home-manager/modules/fastfetch.nix
@@ -6,7 +6,7 @@
     settings = {
       "$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
       logo = {
-        source = ../../assets/kaoru_waguri.jpg;
+        source = ../../assets/kaoruko_waguri.jpg;
         type = "kitty";
         height = 15;
         padding = {
