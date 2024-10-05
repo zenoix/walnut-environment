ede1f3a14caa9f5c3418dc3687351395c0cca8ca
zenoix <41812358+zenoix@users.noreply.github.com>
2024-10-05 15:53:59 +1300

    chore(fastfetch): rename kaoruko_waguri to png

--- home-manager/modules/fastfetch.nix
+++ home-manager/modules/fastfetch.nix
@@ -6,7 +6,7 @@
     settings = {
       "$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
       logo = {
-        source = ../../assets/kaoruko_waguri.jpg;
+        source = ../../assets/kaoruko_waguri.png;
         type = "kitty";
         height = 15;
         padding = {
