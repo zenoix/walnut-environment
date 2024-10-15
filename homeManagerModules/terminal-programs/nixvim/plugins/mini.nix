f10069f4e717a3d449efab3a611bf82e83da6160
zenoix <41812358+zenoix@users.noreply.github.com>
2024-10-16 01:19:11 +1300

    feat(nixvim): add mini.ai

--- homeManagerModules/terminal-programs/nixvim/plugins/mini.nix
+++ homeManagerModules/terminal-programs/nixvim/plugins/mini.nix
@@ -8,33 +8,35 @@
         options = {
           custom_commentstring = {
             __raw = ''
-              function()
-				        return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
-			        end
+                            function()
+              				        return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
+              			        end
             '';
           };
         };
       };
 
       move = {
-		    mappings = {
-		    	# Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
-		    	left = "<S-h>";
-		    	right = "<S-l>";
-		    	down = "<S-j>";
-		    	up = "<S-k>";
+        mappings = {
+          # Move visual selection in Visual mode. Defaults are Alt (Meta) + hjkl.
+          left = "<S-h>";
+          right = "<S-l>";
+          down = "<S-j>";
+          up = "<S-k>";
 
-		    	# Move current line in Normal mode
-		    	line_left = "";
-		    	line_right = "";
-		    	line_down = "";
-		    	line_up = "";
-		    };
+          # Move current line in Normal mode
+          line_left = "";
+          line_right = "";
+          line_down = "";
+          line_up = "";
+        };
       };
 
-      surround = {};
+      surround = { };
 
-      pairs = {};
+      pairs = { };
+
+      ai = { };
 
     };
   };
