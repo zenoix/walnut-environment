d7d260f4befa5c24ead6f461fd08d99f2a0c1d17
zenoix <41812358+zenoix@users.noreply.github.com>
2024-10-16 11:27:22 +1300

    fix(nixvim): fix all hidden files being shown by alpha telescope

--- homeManagerModules/terminal-programs/nixvim/plugins/alpha.nix
+++ homeManagerModules/terminal-programs/nixvim/plugins/alpha.nix
@@ -1,9 +1,10 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   programs.nixvim.plugins.alpha = {
     enable = true;
 
     layout = [
-      { 
+      {
         type = "padding";
         val = 4;
       }
@@ -51,73 +52,45 @@
       }
       {
         type = "group";
-        val = let
-          mkButton = shortcut: cmd: val: hl: {
-            type = "button";
-            inherit val;
-            opts = {
-              inherit hl shortcut;
-              keymap = [
-                "n"
-                shortcut
-                cmd
-                {}
-              ];
-              position = "center";
-              cursor = 0;
-              width = 40;
-              align_shortcut = "right";
-              hl_shortcut = "string";
+        val =
+          let
+            mkButton = shortcut: cmd: val: hl: {
+              type = "button";
+              inherit val;
+              opts = {
+                inherit hl shortcut;
+                keymap = [
+                  "n"
+                  shortcut
+                  cmd
+                  { }
+                ];
+                position = "center";
+                cursor = 0;
+                width = 40;
+                align_shortcut = "right";
+                hl_shortcut = "string";
+              };
             };
-          };
-        in [
-          (
-            mkButton
-            "n"
-            "<CMD>enew<CR>"
-            "  New file"
-            "string"
-          )
-          (
-            mkButton
-            "f"
-            "<CMD>lua require('telescope.builtin').find_files({hidden = true})<CR>"
-            "  Find File"
-            "String"
-          )
-          (
-            mkButton
-            "t"
-            "<CMD>lua require('telescope.builtin').live_grep({hidden = true})<CR>"
-            "󰊄  Find Text"
-            "String"
-          )
-          (
-            mkButton
-            "r"
-            "<CMD>lua require('telescope.builtin').old_files({hidden = true})<CR>"
-            "  Recent Files"
-            "String"
-          )
-          (
-            mkButton
-            "q"
-            "<CMD>q<CR>"
-            "󰅙  Quit Neovim"
-            "String"
-          )
-        ];
+          in
+          [
+            (mkButton "n" "<CMD>enew<CR>" "  New file" "string")
+            (mkButton "f" "<CMD>lua require('telescope.builtin').find_files()<CR>" "  Find File" "String")
+            (mkButton "t" "<CMD>lua require('telescope.builtin').live_grep()<CR>" "󰊄  Find Text" "String")
+            (mkButton "r" "<CMD>lua require('telescope.builtin').old_files()<CR>" "  Recent Files" "String")
+            (mkButton "q" "<CMD>q<CR>" "󰅙  Quit Neovim" "String")
+          ];
       }
-      { 
+      {
         type = "padding";
         val = 2;
       }
       {
         type = "text";
         val = [
-  			  "                                  "
-  			  "Everything not saved will be lost."
-  			  "                                  "
+          "                                  "
+          "Everything not saved will be lost."
+          "                                  "
         ];
         opts = {
           position = "center";
@@ -135,36 +108,36 @@
       noautocmd = true;
       redraw_on_resize = true;
       setup.__raw = ''
-        function()
-        vim.api.nvim_create_autocmd("User", {
-            pattern = "AlphaReady",
-            desc = "hide cursor for alpha",
+          function()
+          vim.api.nvim_create_autocmd("User", {
+              pattern = "AlphaReady",
+              desc = "hide cursor for alpha",
+              callback = function()
+              local hl = vim.api.nvim_get_hl(0, { name = "Cursor" })
+              vim.api.nvim_set_hl(0, "Cursor", vim.tbl_extend("force", hl, { blend = 100 }))
+              vim.opt.guicursor = "a:Cursor/lCursor"
+              end,
+              })
+        vim.api.nvim_create_autocmd("BufUnload", {
+            buffer = 0,
+            desc = "show cursor after alpha",
             callback = function()
             local hl = vim.api.nvim_get_hl(0, { name = "Cursor" })
-            vim.api.nvim_set_hl(0, "Cursor", vim.tbl_extend("force", hl, { blend = 100 }))
-            vim.opt.guicursor = "a:Cursor/lCursor"
+            vim.api.nvim_set_hl(0, "Cursor", vim.tbl_extend("force", hl, { blend = nil }))
+            vim.opt.guicursor = "n-v-c-sm-i-ci-ve-o:block,r-cr:hor20"
             end,
             })
-      vim.api.nvim_create_autocmd("BufUnload", {
-          buffer = 0,
-          desc = "show cursor after alpha",
-          callback = function()
-          local hl = vim.api.nvim_get_hl(0, { name = "Cursor" })
-          vim.api.nvim_set_hl(0, "Cursor", vim.tbl_extend("force", hl, { blend = nil }))
-          vim.opt.guicursor = "n-v-c-sm-i-ci-ve-o:block,r-cr:hor20"
-          end,
-          })
-      vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
-          pattern = { "oil://*" },
-          desc = "show cursor after alpha",
-          callback = function()
-          local hl = vim.api.nvim_get_hl(0, { name = "Cursor" })
-          vim.api.nvim_set_hl(0, "Cursor", vim.tbl_extend("force", hl, { blend = nil }))
-          vim.opt.guicursor = "n-v-c-sm-i-ci-ve-o:block,r-cr:hor20"
-          end,
-          })
-      end
-        '';
+        vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+            pattern = { "oil://*" },
+            desc = "show cursor after alpha",
+            callback = function()
+            local hl = vim.api.nvim_get_hl(0, { name = "Cursor" })
+            vim.api.nvim_set_hl(0, "Cursor", vim.tbl_extend("force", hl, { blend = nil }))
+            vim.opt.guicursor = "n-v-c-sm-i-ci-ve-o:block,r-cr:hor20"
+            end,
+            })
+        end
+      '';
     };
   };
 }
