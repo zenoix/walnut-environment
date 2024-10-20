c72675a75037696e2c298d33e6bdea5c92a2709c
zenoix <41812358+zenoix@users.noreply.github.com>
2024-10-20 22:59:36 +1300

    chore(nixvim): remove useless setting

--- homeManagerModules/terminal-programs/nixvim/plugins/treesitter.nix
+++ homeManagerModules/terminal-programs/nixvim/plugins/treesitter.nix
@@ -2,22 +2,6 @@
   programs.nixvim.plugins.treesitter = {
     enable = true;
     settings = {
-      ensure_installed = [
-        "gitignore"
-        "python"
-        "r"
-        "sql"
-        "yaml"
-        "c"
-        "lua"
-        "vim"
-        "vimdoc"
-        "query"
-        "sql"
-        "julia"
-        "go"
-      ];
-
       highlight.enable = true;
     };
   };
