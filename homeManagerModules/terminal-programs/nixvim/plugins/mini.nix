b29f7678f8386e51ecd74d1d4587b6ed8dd7857a
zenoix <41812358+zenoix@users.noreply.github.com>
2024-10-20 22:50:39 +1300

    style(nixvim): format mini commentstring lua block

--- homeManagerModules/terminal-programs/nixvim/plugins/mini.nix
+++ homeManagerModules/terminal-programs/nixvim/plugins/mini.nix
@@ -8,9 +8,9 @@
         options = {
           custom_commentstring = {
             __raw = ''
-                            function()
-              				        return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
-              			        end
+              function()
+                return require("ts_context_commentstring.internal").calculate_commentstring() or vim.bo.commentstring
+              end
             '';
           };
         };
