6189be88b7729d094a316c4a8097955356f281b7
zenoix <41812358+zenoix@users.noreply.github.com>
2024-11-12 21:48:30 +1300

    chore(nixvim): use unstable pkgs for treesitter context

--- homeManagerModules/terminal-programs/nixvim/plugins/treesitter-context.nix
+++ homeManagerModules/terminal-programs/nixvim/plugins/treesitter-context.nix
@@ -1,6 +1,8 @@
+{ pkgs-unstable, ... }:
 {
   programs.nixvim.plugins.treesitter-context = {
     enable = true;
+    package = pkgs-unstable.vimPlugins.nvim-treesitter-context;
     settings = {
       max_lines = 3;
     };
