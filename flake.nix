6fafb7b8738d1a14190401e7efefd8954dcd5fa6
zenoix <41812358+zenoix@users.noreply.github.com>
2024-11-23 22:55:48 +1300

    chore(flake): use unstable pkgs for spicetify

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
     };
     spicetify-nix = {
       url = "github:Gerg-L/spicetify-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
     };
   };
 
