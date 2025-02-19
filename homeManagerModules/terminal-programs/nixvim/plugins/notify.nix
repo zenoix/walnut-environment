6aaa3f93449faa7340d947c2e23ddd653cb3f8ad
zenoix <41812358+zenoix@users.noreply.github.com>
2025-02-20 09:17:49 +1300

    chore: use unstable nixvim and update migrated notify settings

--- homeManagerModules/terminal-programs/nixvim/plugins/notify.nix
+++ homeManagerModules/terminal-programs/nixvim/plugins/notify.nix
@@ -1,24 +1,26 @@
 {
   programs.nixvim.plugins.notify = {
     enable = true;
-    maxHeight = {
-      __raw = ''
-        function()
+    settings = {
+      maxHeight = {
+        __raw = ''
+          function()
           return math.floor(vim.o.lines * 0.75)
-        end
-      '';
-    };
-    maxWidth = {
-      __raw = ''
-        function()
+          end
+        '';
+      };
+      maxWidth = {
+        __raw = ''
+          function()
           return math.floor(vim.o.columns * 0.75)
+          end
+        '';
+      };
+      onOpen = ''
+        function(win)
+        vim.api.nvim_win_set_config(win, { zindex = 100 })
         end
       '';
     };
-    onOpen = ''
-      function(win)
-        vim.api.nvim_win_set_config(win, { zindex = 100 })
-      end
-    '';
   };
 }
