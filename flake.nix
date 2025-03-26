af6d3ff8dc9383bb7045ef4f9b4b31446b002ee3
zenoix <41812358+zenoix@users.noreply.github.com>
2025-03-26 19:59:16 +1300

    feat(flake): add ags as an input

--- flake.nix
+++ flake.nix
@@ -6,6 +6,8 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
+    ags.url = "github:aylur/ags";
+
     home-manager = {
       url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -35,6 +37,7 @@
       nixpkgs,
       nixpkgs-unstable,
       home-manager,
+      ags,
       stylix,
       ...
     }@inputs:
