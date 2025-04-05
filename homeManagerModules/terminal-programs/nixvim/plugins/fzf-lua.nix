61a6b5c80f5cb00ca21b5bba6634f9f84e3964a1
zenoix <41812358+zenoix@users.noreply.github.com>
2025-04-05 16:46:10 +1300

    feat(nixvim): add fzf-lua files and grep options

--- homeManagerModules/terminal-programs/nixvim/plugins/fzf-lua.nix
+++ homeManagerModules/terminal-programs/nixvim/plugins/fzf-lua.nix
@@ -1,4 +1,4 @@
-{ pkgs-unstable, ... }:
+{ pkgs-unstable, lib, ... }:
 {
   programs.nixvim.plugins.fzf-lua = {
     enable = true;
@@ -9,6 +9,51 @@
         preview = {
           default = "bat";
         };
+      };
+
+      files = {
+        fd_opts = lib.strings.concatStringsSep " " [
+          "--color=never" # Never show color
+          "--hidden" # Show hidden files
+          "--type f" # Find files
+          "--type l" # Find symlinks
+
+          # Exclude some patterns from search
+          "--exclude .git"
+          "--exclude '**/.devbox/*'"
+          "--exclude '**/.vscode/*'"
+          "--exclude '**/venv/*'"
+          "--exclude '**/.venv/*'"
+        ];
+        follow = true;
+      };
+
+      grep = {
+        rg_opts = lib.strings.concatStringsSep " " [
+          "--column" # Show column numbers
+          "--no-heading" # Don't group matches by each file
+          "--with-filename" # Print the file path with the matched lines
+          "--line-number" # Show line numbers
+          "--column" # Show column numbers
+          "--smart-case" # Smart case search
+          "--color=always" # Always use color
+
+          # Exclude some patternnixs from search
+          "--glob=!**/.git/*"
+          "--glob=!**/.devbox/*"
+          "--glob=!**/.venv/*"
+          "--glob=!**/venv/*"
+          "--glob=!**/.idea/*"
+          "--glob=!**/.vscode/*"
+          "--glob=!**/build/*"
+          "--glob=!**/dist/*"
+          "--glob=!**/yarn.lock"
+          "--glob=!**/package-lock.json"
+
+          "-e"
+        ];
+        follow = true;
+        hidden = true;
       };
     };
 
