863d8e86015558b9e0f41ac5821202f09aedda89
zenoix <41812358+zenoix@users.noreply.github.com>
2025-04-21 11:55:49 +1200

    feat(nixvim): update harpoon configuration to use the new harpoon2 version

--- homeManagerModules/terminal-programs/nixvim/plugins/harpoon.nix
+++ homeManagerModules/terminal-programs/nixvim/plugins/harpoon.nix
@@ -2,24 +2,74 @@
   programs.nixvim.plugins.harpoon = {
     enable = true;
     enableTelescope = false;
-    keymaps = {
-      addFile = "<leader>h";
-      toggleQuickMenu = "<leader>ph";
-      navFile = {
-        "1" = "<leader>1";
-        "2" = "<leader>2";
-        "3" = "<leader>3";
-        "4" = "<leader>4";
-        "5" = "<leader>5";
-        "6" = "<leader>6";
-        "7" = "<leader>7";
-        "8" = "<leader>8";
-        "9" = "<leader>9";
+    settings = {
+      menu = {
+        height = 20;
+        width = 80;
       };
     };
-    menu = {
-      height = 20;
-      width = 80;
-    };
   };
+
+  programs.nixvim.keymaps = [
+    {
+      mode = "n";
+      key = "<leader>h";
+      action.__raw = "function() require'harpoon':list():add() end";
+    }
+    {
+      mode = "n";
+      key = "<leader>ph";
+      action.__raw = ''
+        function() 
+          local hp = require'harpoon'
+          hp.ui:toggle_quick_menu(hp:list())
+        end
+      '';
+    }
+    {
+      mode = "n";
+      key = "<leader>1";
+      action.__raw = "function() require'harpoon':list():select(1) end";
+    }
+    {
+      mode = "n";
+      key = "<leader>2";
+      action.__raw = "function() require'harpoon':list():select(2) end";
+    }
+    {
+      mode = "n";
+      key = "<leader>3";
+      action.__raw = "function() require'harpoon':list():select(3) end";
+    }
+    {
+      mode = "n";
+      key = "<leader>4";
+      action.__raw = "function() require'harpoon':list():select(4) end";
+    }
+    {
+      mode = "n";
+      key = "<leader>5";
+      action.__raw = "function() require'harpoon':list():select(5) end";
+    }
+    {
+      mode = "n";
+      key = "<leader>6";
+      action.__raw = "function() require'harpoon':list():select(6) end";
+    }
+    {
+      mode = "n";
+      key = "<leader>7";
+      action.__raw = "function() require'harpoon':list():select(7) end";
+    }
+    {
+      mode = "n";
+      key = "<leader>8";
+      action.__raw = "function() require'harpoon':list():select(8) end";
+    }
+    {
+      mode = "n";
+      key = "<leader>9";
+      action.__raw = "function() require'harpoon':list():select(9) end";
+    }
+  ];
 }
