d4a1cf92b56c7a2b73db4f33feee487423a4d85e
zenoix <41812358+zenoix@users.noreply.github.com>
2025-04-26 16:59:58 +1200

    feat(nixvim): configure noice

--- homeManagerModules/terminal-programs/nixvim/plugins/noice.nix
+++ homeManagerModules/terminal-programs/nixvim/plugins/noice.nix
@@ -1,5 +1,32 @@
 {
   programs.nixvim.plugins.noice = {
+
     enable = true;
+
+    settings = {
+      cmdline = {
+        view = "cmdline";
+      };
+
+      lsp = {
+        documentation.enabled = false;
+        progress.enabled = false;
+        signature.enabled = false;
+        hover.enabled = false;
+      };
+
+      messages = {
+        enabled = false;
+      };
+
+      notify = {
+        enabled = false;
+      };
+
+      presets = {
+        bottom_search = true;
+        command_palette = true;
+      };
+    };
   };
 }
