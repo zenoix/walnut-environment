c3d7ab888404128fbb2bcf240ec4b998d8e5943b
zenoix <41812358+zenoix@users.noreply.github.com>
2025-04-29 14:09:42 +1200

    feat(git): extend git config

--- homeManagerModules/coding/git.nix
+++ homeManagerModules/coding/git.nix
@@ -20,8 +20,78 @@
       };
 
       extraConfig = {
+        branch = {
+          sort = "-committerdate";
+        };
+
+        core = {
+          whitespace = "error";
+        };
+
         init.defaultBranch = "main";
-        pull.rebase = true;
+
+        interactive = {
+          diffFilter = "delta --color-only";
+        };
+
+        log = {
+          abbreCommit = true;
+          graphColors = "blue,yellow,cyan,magenta,green,red";
+        };
+
+        merge.conflictstyle = "zdiff3";
+
+        push = {
+          autoSetupRemote = true;
+          default = "current";
+        };
+
+        pull = {
+          default = "current";
+          rebase = true;
+        };
+
+        rebase = {
+          autoStash = true;
+          missingCommitsCheck = "warn";
+        };
+
+        status = {
+          branch = true;
+          showStash = true;
+          showUntrackedFiles = "all";
+        };
+
+        tag = {
+          sort = "-taggerdate";
+        };
+
+        url = {
+          "git@github.com:zenoix/" = {
+            insteadOf = "z/";
+            "git@github.com:" = {
+              insteadOf = "gh:";
+            };
+          };
+        };
+
+        color = {
+          branch = {
+            current = "magenta";
+            local = "default";
+            remote = "yellow";
+            upstream = "green";
+            plain = "blue";
+          };
+
+          "decorate" = {
+            HEAD = "red";
+            branch = "blue";
+            tag = "yellow";
+            remoteBranch = "magenta";
+          };
+        };
+
       };
 
       delta = {
