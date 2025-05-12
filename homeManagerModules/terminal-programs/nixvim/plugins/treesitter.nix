f437a0e4e31525a6f5f5cb0649cdc22a07199d34
zenoix <41812358+zenoix@users.noreply.github.com>
2025-05-13 00:19:20 +1200

    fix(nixvim): fix treesitter parsing issue by updating and seperately installing some grammars

--- homeManagerModules/terminal-programs/nixvim/plugins/treesitter.nix
+++ homeManagerModules/terminal-programs/nixvim/plugins/treesitter.nix
@@ -1,8 +1,15 @@
+{ pkgs-unstable, ... }:
 {
   programs.nixvim.plugins.treesitter = {
     enable = true;
+    package = pkgs-unstable.vimPlugins.nvim-treesitter;
     settings = {
       highlight.enable = true;
+      ensure_installed = [
+        "vim"
+        "vimdoc"
+        "luadoc"
+      ];
     };
   };
 }
