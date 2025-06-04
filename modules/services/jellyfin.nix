fff9efb11d9bc76ab299b966dc0c3150d2df37e9
zenoix <41812358+zenoix@users.noreply.github.com>
2025-06-04 20:44:39 +1200

    fix(jellyfin): use correct openFirewall option name

--- modules/services/jellyfin.nix
+++ modules/services/jellyfin.nix
@@ -12,7 +12,7 @@
   config = lib.mkIf config.walnut.jellyfin.enable {
     services.jellyfin = {
       enable = true;
-      openFilewall = true;
+      openFirewall = true;
     };
     environment.systemPackages = with pkgs; [
       jellyfin
