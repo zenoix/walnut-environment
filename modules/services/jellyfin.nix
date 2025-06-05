cc9d833cb4ad9057a214b62d14b893d4d73f390d
zenoix <41812358+zenoix@users.noreply.github.com>
2025-06-05 18:40:57 +1200

    feat(jellyfin): change jellyfin's service user

--- modules/services/jellyfin.nix
+++ modules/services/jellyfin.nix
@@ -2,6 +2,7 @@
   pkgs,
   lib,
   config,
+  server,
   ...
 }:
 {
@@ -13,6 +14,7 @@
     services.jellyfin = {
       enable = true;
       openFirewall = true;
+      user = server.name;
     };
     environment.systemPackages = with pkgs; [
       jellyfin
