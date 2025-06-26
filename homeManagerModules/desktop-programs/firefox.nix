4b873b500928670b329f80a11841a187959a71e8
zenoix <41812358+zenoix@users.noreply.github.com>
2025-06-27 11:37:02 +1200

    feat(firefox): make firefox configuration more declarative

--- homeManagerModules/desktop-programs/firefox.nix
+++ homeManagerModules/desktop-programs/firefox.nix
@@ -13,6 +13,20 @@
   config = lib.mkIf config.walnutHome.firefox.enable {
     programs.firefox = {
       enable = true;
+
+      policies = {
+        DisableTelemetry = true;
+        DisableFirefoxStudies = true;
+        EnableTrackingProtection = {
+          Value = true;
+          Locked = true;
+          Cryptomining = true;
+          Fingerprinting = true;
+        };
+        DisablePocket = true;
+        OverridePostUpdatePage = "";
+      };
+
       profiles =
         let
           extensionsConfig = {
@@ -28,21 +42,38 @@
                 vimium
                 youtube-nonstop
               ];
+
+              force = true;
             };
+
           };
 
-          customCSSConfig = {
+          otherSettings = {
+            userChrome = builtins.readFile ../../nonNix/firefox/userChrome.css;
+
             settings = {
               "toolkit.legacyUserProfileCustomizations.stylesheets" = true; # enable userChrome.css
+
+              "extensions.autoDisableScopes" = 0;
+              "extensions.htmlaboutaddons.recommendations.enabled" = false;
+
+              "signon.rememberSignons" = false;
+
+              "browser.aboutConfig.showWarning" = false;
+              "browser.urlbar.suggest.topsites" = false;
+              "browser.urlbar.suggest.trending" = false;
+              "browser.urlbar.suggest.quicksuggest.nonsponsored" = false;
+              "browser.urlbar.suggest.quicksuggest.sponsored" = false;
+              "browser.urlbar.suggest.yelp" = false;
             };
-            userChrome = builtins.readFile ../../nonNix/firefox/userChrome.css;
           };
 
-          commonSettings = extensionsConfig // customCSSConfig;
+          commonSettings = extensionsConfig // otherSettings;
         in
         {
           default = commonSettings // {
             id = 0;
+            isDefault = true;
           };
           default-release = commonSettings // {
             id = 1;
