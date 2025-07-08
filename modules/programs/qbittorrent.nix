1f15a7c7d3102f1886ff820dee20b943859d5c98
zenoix <41812358+zenoix@users.noreply.github.com>
2025-07-08 19:52:18 +1200

    feat(qbittorrent): create systemd service for qbittorrent

--- modules/programs/qbittorrent.nix
+++ modules/programs/qbittorrent.nix
@@ -11,5 +11,13 @@
 
   config = lib.mkIf config.walnut.qbittorrent.enable {
     environment.systemPackages = [ pkgs-unstable.qbittorrent-enhanced-nox ];
+
+    systemd = {
+      packages = [ pkgs-unstable.qbittorrent-enhanced-nox ];
+      services."qbittorrent-nox" = {
+        overrideStrategy = "asDropin";
+        wantedBy = [ "multi-user.target" ];
+      };
+    };
   };
 }
