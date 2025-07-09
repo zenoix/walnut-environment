795d3c0724fc5993f54f2589df4e4f20803c310a
zenoix <41812358+zenoix@users.noreply.github.com>
2025-07-10 09:54:00 +1200

    feat(networking): use cloudflare's dns

--- modules/services/networking.nix
+++ modules/services/networking.nix
@@ -13,6 +13,14 @@
     # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
     # Enable networking
-    networking.networkmanager.enable = true;
+    networking = {
+      networkmanager.enable = true;
+      nameservers = [
+        "1.1.1.1"
+        "1.0.0.1"
+        "2606:4700:4700::1111"
+        "2606:4700:4700::1001"
+      ];
+    };
   };
 }
