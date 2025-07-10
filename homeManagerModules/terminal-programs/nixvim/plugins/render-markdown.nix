ba04e11929e61962e3700778d815016bcf563fe1
zenoix <41812358+zenoix@users.noreply.github.com>
2025-07-10 14:45:25 +1200

    refactor(nixvim): use nixvim render-markdown instead of fetching from github

--- homeManagerModules/terminal-programs/nixvim/plugins/render-markdown.nix
+++ homeManagerModules/terminal-programs/nixvim/plugins/render-markdown.nix
@@ -1,17 +1,9 @@
 { pkgs, ... }:
 {
   programs.nixvim = {
-    extraPlugins = [
-      (pkgs.vimUtils.buildVimPlugin {
-        name = "render-markdown";
-        src = pkgs.fetchFromGitHub {
-          owner = "MeanderingProgrammer";
-          repo = "render-markdown.nvim";
-          rev = "e1a2289106a9ce5ea1b20b78c90aef4d890950b7";
-          hash = "sha256-9ash/ev12Elee2YIRiX5KCCR55jN+DA2dshQ7Uwjrpc=";
-        };
-      })
-    ];
+    plugins.render-markdown = {
+      enable = true;
+    };
 
     extraPackages = [ pkgs.python312Packages.pylatexenc ];
   };
