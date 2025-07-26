02dcd5a0ded2be9c27774a02aa466302feab62fb
zenoix <41812358+zenoix@users.noreply.github.com>
2025-07-26 23:33:06 +1200

    refactor(nixvim): use nix default.nix for nixvim config

--- homeManagerModules/terminal-programs/default.nix
+++ homeManagerModules/terminal-programs/default.nix
@@ -6,12 +6,13 @@
     ./fzf.nix
     ./gh-cli.nix
     ./jujutsu.nix
-    ./nixvim/nixvim.nix
     ./ripgrep.nix
     ./starship.nix
     ./tmux.nix
     ./yazi.nix
     ./zoxide.nix
     ./zsh.nix
+
+    ./nixvim
   ];
 }
