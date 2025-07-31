afd3146f704fceffc7ba7cc008adc4f3ee880685
zenoix <41812358+zenoix@users.noreply.github.com>
2025-07-31 16:32:00 +1200

    fix(waybar): set battery full-at to 96% due to old laptop battery

--- homeManagerModules/wm/waybar.nix
+++ homeManagerModules/wm/waybar.nix
@@ -224,6 +224,7 @@
                 warning = 30;
                 critical = 15;
               };
+              full-at = 96;
               format = "{icon} {capacity}%";
               format-charging = "󰂄 {capacity}%";
               format-plugged = " {capacity}%";
