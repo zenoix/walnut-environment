48d8c455d34f57e13b8c7c10fbc572decebdde40
zenoix <41812358+zenoix@users.noreply.github.com>
2025-08-11 00:44:28 +1200

    revert(tlp): "chore(tlp): adjust settings"
    This reverts commit eb08659dd86d10980140cae21f0bd2824fbff28c.

--- modules/services/tlp.nix
+++ modules/services/tlp.nix
@@ -10,18 +10,6 @@
       settings = {
         START_CHARGE_THRESH_BAT0 = 40;
         STOP_CHARGE_THRESH_BAT0 = 80;
-
-        CPU_SCALING_GOVERNOR_ON_AC = "performance";
-        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-
-        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-        CPU_ENERGY_PERF_POLICY_ON_AC = "balance_performance";
-
-        CPU_MIN_PERF_ON_AC = 0;
-        CPU_MAX_PERF_ON_AC = 100;
-        CPU_MIN_PERF_ON_BAT = 0;
-        CPU_MAX_PERF_ON_BAT = 40;
-
         DEVICES_TO_DISABLE_ON_STARTUP = "bluetooth";
       };
     };
