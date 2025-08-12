ef12fb6423559f29ba00478789d776612da5a26f
zenoix <41812358+zenoix@users.noreply.github.com>
2025-08-12 14:08:32 +1200

    feat: make gcc and g++ strict about warnings

--- homeManagerModules/terminal-programs/aliases.nix
+++ homeManagerModules/terminal-programs/aliases.nix
@@ -122,8 +122,8 @@
         gwtls = "git worktree list";
         gwtmv = "git worktree move";
         gwtrm = "git worktree remove";
-        gcc = "gcc -pedantic-errors";
-        "g++" = "g++ -pedantic-errors";
+        gcc = "gcc -pedantic-errors -Wall -Weffc++ -Wextra -Wconversion -Wsign-conversion -Werror";
+        "g++" = "g++ -pedantic-errors -Wall -Weffc++ -Wextra -Wconversion -Wsign-conversion -Werror";
         l = "ls -lah --group-directories-first";
         la = "ls -lAh --group-directories-first";
         ll = "ls -lh --group-directories-first";
