8acf553a4e6f6a38befb33034f61555a37435bdf
zenoix <41812358+zenoix@users.noreply.github.com>
2025-08-16 01:32:09 +1200

    feat(hosts): rotate right monitor on desktop

--- hosts/walnutDesktop/home.nix
+++ hosts/walnutDesktop/home.nix
@@ -20,7 +20,7 @@
       enable = true;
       monitorSetup = [
         "DP-2,preferred,0x0,1,bitdepth,10"
-        "DP-3,preferred,2560x0,1,bitdepth,10"
+        "DP-3,preferred,2560x-560,1,bitdepth,10,transform,1"
       ];
     };
     hyprlock.enable = true;
