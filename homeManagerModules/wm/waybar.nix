75e4534a053143621330213b0e638a6c33ca097a
zenoix <41812358+zenoix@users.noreply.github.com>
2025-08-17 12:52:46 +1200

    refactor(waybar): use catppuccin-nix colors instead of stylix

--- homeManagerModules/wm/waybar.nix
+++ homeManagerModules/wm/waybar.nix
@@ -116,15 +116,11 @@
                 mode-mon-col = 3;
                 on-scroll = 1;
                 on-click-right = "mode";
-                format =
-                  let
-                    inherit (config.lib.stylix) colors;
-                  in
-                  {
-                    months = "<span color='#${colors.base0A}'><b>{}</b></span>";
-                    weekdays = "<span color='#${colors.base0E}'><b>{}</b></span>";
-                    today = "<span color='#${colors.base08}'><b>{}</b></span>";
-                  };
+                format = {
+                  months = "<span color='#f9e2af'><b>{}</b></span>";
+                  weekdays = "<span color='#cba6f7'><b>{}</b></span>";
+                  today = "<span color='#f38ba8'><b>{}</b></span>";
+                };
               };
               actions = {
                 on-click-right = "mode";
@@ -144,12 +140,7 @@
               format-linked = "󰈀 {ifname} (No IP)";
               format-disconnected = "󰖪 ";
               tooltip-format-disconnected = "Disconnected";
-              format-alt =
-                let
-
-                  inherit (config.lib.stylix) colors;
-                in
-                "<span foreground='#${colors.base0C}'> {bandwidthDownBytes}</span> <span foreground='#${colors.base09}'> {bandwidthUpBytes}</span>";
+              format-alt = "<span foreground='#94e2d5'> {bandwidthDownBytes}</span> <span foreground='#fab387'> {bandwidthUpBytes}</span>";
               interval = 2;
             };
 
@@ -299,218 +290,214 @@
           };
         };
 
-        style =
-          let
-            inherit (config.lib.stylix) colors;
-          in
-          ''
-            * {
-              border: none;
-              border-radius: 0;
-              /* `otf-font-awesome` is required to be installed for icons */
-              font-size: 12px;
-              font-family: JetBrains Mono Nerd Font;
-              font-weight: bold;
-              min-height: 18px;
-            }
+        style = ''
+          * {
+            border: none;
+            border-radius: 0;
+            /* `otf-font-awesome` is required to be installed for icons */
+            font-size: 12px;
+            font-family: JetBrains Mono Nerd Font;
+            font-weight: bold;
+            min-height: 18px;
+          }
 
-            window#waybar {
-              background: #${colors.base00};
-              border-radius: 5px;
-            }
+          window#waybar {
+            background: @base;
+            border-radius: 5px;
+          }
 
-            window#waybar.hidden {
-              opacity: 0.2;
-            }
+          window#waybar.hidden {
+            opacity: 0.2;
+          }
 
-            tooltip {
-              border-radius: 8px;
-              border-width: 0px;
-              background: #${colors.base00};
-            }
+          tooltip {
+            border-radius: 8px;
+            border-width: 0px;
+            background: @base;
+          }
 
-            tooltip label {
-              color: #${colors.base05};
-            }
+          tooltip label {
+            color: @text;
+          }
 
-            #group1,
-            #power {
-              padding-left: 10px;
-              padding-right: 10px;
-            }
+          #group1,
+          #power {
+            padding-left: 10px;
+            padding-right: 10px;
+          }
 
-            #group1 {
-              margin-right: 8px;
-            }
+          #group1 {
+            margin-right: 8px;
+          }
 
-            #network {
-              padding-left: 5px;
-              padding-right: 2px;
-            }
+          #network {
+            padding-left: 5px;
+            padding-right: 2px;
+          }
 
-            #bluetooth,
-            #pulseaudio,
-            #pulseaudio#microphone,
-            #idle_inhibitor {
-              padding-left: 5px;
-              padding-right: 5px;
-            }
+          #bluetooth,
+          #pulseaudio,
+          #pulseaudio#microphone,
+          #idle_inhibitor {
+            padding-left: 5px;
+            padding-right: 5px;
+          }
 
-            #custom-power,
-            #custom-quit,
-            #custom-lock,
-            #custom-reboot {
-              padding-left: 10px;
-              padding-right: 10px;
-              border-radius: 10px;
-              border-width: 0px;
-              transition: none;
-            }
+          #custom-power,
+          #custom-quit,
+          #custom-lock,
+          #custom-reboot {
+            padding-left: 10px;
+            padding-right: 10px;
+            border-radius: 10px;
+            border-width: 0px;
+            transition: none;
+          }
 
-            #custom-power {
-              color: #f5c2e7;
-            }
+          #custom-power {
+            color: #f5c2e7;
+          }
 
-            #custom-quit {
-              padding-left: 10px;
-            }
+          #custom-quit {
+            padding-left: 10px;
+          }
 
-            #group2 {
-              padding-left: 16px;
-              padding-right: 16px;
-              border-radius: 10px;
-              margin-right: 8px;
-              transition: none;
-            }
+          #group2 {
+            padding-left: 16px;
+            padding-right: 16px;
+            border-radius: 10px;
+            margin-right: 8px;
+            transition: none;
+          }
 
-            #battery,
-            #backlight {
-              padding-left: 5px;
-              padding-right: 5px;
-            }
+          #battery,
+          #backlight {
+            padding-left: 5px;
+            padding-right: 5px;
+          }
 
-            #backlight {
-              transition: none;
-              color: #${colors.base05};
-            }
+          #backlight {
+            transition: none;
+            color: @text;
+          }
 
-            #battery {
-              transition: none;
-              color: #${colors.base05};
-            }
+          #battery {
+            transition: none;
+            color: @text;
+          }
 
-            /* #battery.charging {
-              color: #${colors.base00};
-              background-color: #${colors.base0B};
-            } */
+          /* #battery.charging {
+            color: @base;
+            background-color: @green;
+          } */
 
-            #battery.warning:not(.charging) {
-              color: #${colors.base0A};
-            }
+          #battery.warning:not(.charging) {
+            color: @yellow;
+          }
 
-            #battery.critical:not(.charging) {
-              color: #${colors.base08};
-              animation-name: blink;
-              animation-duration: 0.5s;
-              animation-timing-function: linear;
-              animation-iteration-count: infinite;
-              animation-direction: alternate;
-            }
+          #battery.critical:not(.charging) {
+            color: @red;
+            animation-name: blink;
+            animation-duration: 0.5s;
+            animation-timing-function: linear;
+            animation-iteration-count: infinite;
+            animation-direction: alternate;
+          }
 
-            #workspaces {
-              margin-right: 8px;
-              padding-left: 8px;
-              padding-right: 8px;
-              border-radius: 10px;
-              transition: none;
-            }
+          #workspaces {
+            margin-right: 8px;
+            padding-left: 8px;
+            padding-right: 8px;
+            border-radius: 10px;
+            transition: none;
+          }
 
-            #workspaces button {
-              box-shadow: none;
-              text-shadow: none;
-              padding: 0px;
-              border-radius: 8px;
-              margin-top: 4px;
-              margin-bottom: 4px;
-              margin-left: 0px;
-              padding-left: 4px;
-              padding-right: 4px;
-              margin-right: 0px;
-              color: #${colors.base04};
-              animation: ws_normal 20s ease-in-out 1;
-            }
+          #workspaces button {
+            box-shadow: none;
+            text-shadow: none;
+            padding: 0px;
+            border-radius: 8px;
+            margin-top: 4px;
+            margin-bottom: 4px;
+            margin-left: 0px;
+            padding-left: 4px;
+            padding-right: 4px;
+            margin-right: 0px;
+            color: @surface2;
+            animation: ws_normal 20s ease-in-out 1;
+          }
 
-            #workspaces button.persistent {
-              color: #${colors.base04};
-            }
+          #workspaces button.persistent {
+            color: @surface2;
+          }
 
-            #workspaces button.active {
-              /* background: #2B2F38; */
-              color: #f5c2e7;
-              /* margin-left: 1px; 
-              padding-left: 4px;
-              padding-right: 8px;
-              margin-right: 1px; */
-              /* animation: ws_active 20s ease-in-out 1; */
-              /* transition: all 0.4s cubic-bezier(.55,-0.68,.48,1.682); */
-            }
+          #workspaces button.active {
+            /* background: #2B2F38; */
+            color: #f5c2e7;
+            /* margin-left: 1px; 
+            padding-left: 4px;
+            padding-right: 8px;
+            margin-right: 1px; */
+            /* animation: ws_active 20s ease-in-out 1; */
+            /* transition: all 0.4s cubic-bezier(.55,-0.68,.48,1.682); */
+          }
 
-            #workspaces button:hover {
-              /* background: @wb-hvr-bg; */
-              color: #d5a5aa;
-              animation: ws_hover 20s ease-in-out 1;
-              transition: all 0.3s cubic-bezier(0.55, -0.68, 0.48, 1.682);
-            }
+          #workspaces button:hover {
+            /* background: @wb-hvr-bg; */
+            color: #d5a5aa;
+            animation: ws_hover 20s ease-in-out 1;
+            transition: all 0.3s cubic-bezier(0.55, -0.68, 0.48, 1.682);
+          }
 
-            #language {
-              padding-left: 12px;
-              padding-right: 12px;
-              border-radius: 10px;
-              transition: none;
-              color: #${colors.base05};
-            }
+          #language {
+            padding-left: 12px;
+            padding-right: 12px;
+            border-radius: 10px;
+            transition: none;
+            color: @text;
+          }
 
-            #keyboard-state {
-              margin-right: 8px;
-              padding-right: 16px;
-              border-radius: 0px 10px 10px 0px;
-              transition: none;
-              color: #ffffff;
-              background: #383c4a;
-            }
+          #keyboard-state {
+            margin-right: 8px;
+            padding-right: 16px;
+            border-radius: 0px 10px 10px 0px;
+            transition: none;
+            color: #ffffff;
+            background: #383c4a;
+          }
 
-            #submap {
-              padding-left: 16px;
-              padding-right: 16px;
-              border-radius: 10px;
-              transition: none;
-              color: #ffffff;
-              background: #383c4a;
-            }
+          #submap {
+            padding-left: 16px;
+            padding-right: 16px;
+            border-radius: 10px;
+            transition: none;
+            color: #ffffff;
+            background: #383c4a;
+          }
 
-            #clock {
-              padding-left: 16px;
-              padding-right: 16px;
-              border-radius: 10px 10px 10px 10px;
-              transition: none;
-              color: #${colors.base05};
-            }
+          #clock {
+            padding-left: 16px;
+            padding-right: 16px;
+            border-radius: 10px 10px 10px 10px;
+            transition: none;
+            color: @text;
+          }
 
-            #tray {
-              padding-left: 16px;
-              padding-right: 16px;
-              border-radius: 10px;
-              transition: none;
-              color: #${colors.base05};
-            }
+          #tray {
+            padding-left: 16px;
+            padding-right: 16px;
+            border-radius: 10px;
+            transition: none;
+            color: @text;
+          }
 
-            @keyframes blink {
-              to {
-                background-color: #ffffff;
-                color: #000000;
-              }
+          @keyframes blink {
+            to {
+              background-color: #ffffff;
+              color: #000000;
             }
-          '';
+          }
+        '';
       };
     })
 
