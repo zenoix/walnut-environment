41cf2f701876181100fd11498d190f0edbc8efcb
zenoix <41812358+zenoix@users.noreply.github.com>
2025-08-17 19:02:08 +1200

    fix(dunst): remove stylix color

--- homeManagerModules/wm/dunst.nix
+++ homeManagerModules/wm/dunst.nix
@@ -14,40 +14,35 @@
 
     services.dunst = {
       enable = true;
-      settings =
-        let
-          inherit (config.lib.stylix) colors;
-        in
-        {
-          global = {
-            corner_radius = 10;
-            font = lib.mkForce "JetBrainsMono Nerd Font 10";
-            frame_color = "${colors.base0E}";
-            frame_width = 2;
-            gap_size = 10;
-            height = 300;
-            horizontal_padding = lib.mkForce 12;
-            # icon_theme = "Pop,Adwaita,Papirus-Dark";
-            icon_path = "${pkgs.pop-icon-theme}/share/icons/Pop/24x24/actions:${pkgs.pop-icon-theme}/share/icons/Pop/24x24/animations:${pkgs.pop-icon-theme}/share/icons/Pop/24x24/apps:${pkgs.pop-icon-theme}/share/icons/Pop/24x24/categories:${pkgs.pop-icon-theme}/share/icons/Pop/24x24/devices:${pkgs.pop-icon-theme}/share/icons/Pop/24x24/emblems:${pkgs.pop-icon-theme}/share/icons/Pop/24x24/emotes:${pkgs.pop-icon-theme}/share/icons/Pop/24x24/mimetypes:${pkgs.pop-icon-theme}/share/icons/Pop/24x24/panel:${pkgs.pop-icon-theme}/share/icons/Pop/24x24/places:${pkgs.pop-icon-theme}/share/icons/Pop/24x24/status";
-            idle_threshold = 120;
-            indicate_hidden = true;
-            notification_limit = 5;
-            offset = "30x30";
-            origin = "top-right";
-            padding = lib.mkForce 12;
-            separator_color = lib.mkForce "frame";
-            transparency = 0;
-            width = 300;
-          };
-
-          urgence_low = {
-            timeout = 10;
-          };
-
-          urgency_normal = {
-            timeout = 10;
-          };
+      settings = {
+        global = {
+          corner_radius = 10;
+          font = lib.mkForce "JetBrainsMono Nerd Font 10";
+          frame_width = 2;
+          gap_size = 10;
+          height = 300;
+          horizontal_padding = lib.mkForce 12;
+          # icon_theme = "Pop,Adwaita,Papirus-Dark";
+          icon_path = "${pkgs.pop-icon-theme}/share/icons/Pop/24x24/actions:${pkgs.pop-icon-theme}/share/icons/Pop/24x24/animations:${pkgs.pop-icon-theme}/share/icons/Pop/24x24/apps:${pkgs.pop-icon-theme}/share/icons/Pop/24x24/categories:${pkgs.pop-icon-theme}/share/icons/Pop/24x24/devices:${pkgs.pop-icon-theme}/share/icons/Pop/24x24/emblems:${pkgs.pop-icon-theme}/share/icons/Pop/24x24/emotes:${pkgs.pop-icon-theme}/share/icons/Pop/24x24/mimetypes:${pkgs.pop-icon-theme}/share/icons/Pop/24x24/panel:${pkgs.pop-icon-theme}/share/icons/Pop/24x24/places:${pkgs.pop-icon-theme}/share/icons/Pop/24x24/status";
+          idle_threshold = 120;
+          indicate_hidden = true;
+          notification_limit = 5;
+          offset = "30x30";
+          origin = "top-right";
+          padding = lib.mkForce 12;
+          separator_color = lib.mkForce "frame";
+          transparency = 0;
+          width = 300;
         };
+
+        urgence_low = {
+          timeout = 10;
+        };
+
+        urgency_normal = {
+          timeout = 10;
+        };
+      };
     };
   };
 }
