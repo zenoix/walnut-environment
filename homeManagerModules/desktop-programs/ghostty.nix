7a127a8b515ab958b298d6580f69c27d26e10966
zenoix <41812358+zenoix@users.noreply.github.com>
2025-08-21 15:12:06 +1200

    chore(ghostty): force gtk single instance detection

--- homeManagerModules/desktop-programs/ghostty.nix
+++ homeManagerModules/desktop-programs/ghostty.nix
@@ -21,6 +21,7 @@
         font-family = "Monaspace Neon";
         font-size = 10;
         font-feature = "calt,ss01,ss02,ss03,ss04,ss06,ss07,ss08,ss09,liga,cv01 2";
+        gtk-single-instance = true;
         window-padding-x = 5;
         window-padding-y = "7,0";
         resize-overlay = "never";
