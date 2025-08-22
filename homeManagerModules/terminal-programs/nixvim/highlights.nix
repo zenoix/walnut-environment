2e07ece3c7f86f975eb9801cef956cd8ea4e594a
zenoix <41812358+zenoix@users.noreply.github.com>
2025-08-22 19:08:54 +1200

    chore(nixvim): remove hacky catppuccin telescope highlights I used

--- homeManagerModules/terminal-programs/nixvim/highlights.nix
+++ homeManagerModules/terminal-programs/nixvim/highlights.nix
@@ -21,25 +21,6 @@
             fg = "#${colors.base0E}";
             bold = true;
           };
-
-          TelescopeSelectionCaret.fg = "#${mauve}";
-          TelescopePromptPrefix.fg = "#${mauve}";
-
-          # HACK: Temporary fix for fzf-lua's broken telescope highlights
-          TelescopeSelection = {
-            fg = "#f2cdcd";
-            bold = true;
-          };
-          TelescopeBorder.fg = "#89b4fa";
-          TelescopeMatching.fg = "#89b4fa";
-          TelescopePreviewTitle.fg = "#a6e3a1";
-          TelescopePromptNormal.fg = "#cdd6f4";
-          TelescopePromptTitle.fg = "#f38ba8";
         };
-
-      # HACK: Temporary fix for fzf-lua's broken telescope highlights
-      extraConfigLuaPost = ''
-        vim.api.nvim_set_hl(0, "TelescopeNormal", {})
-      '';
     };
 }
