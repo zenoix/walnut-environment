0af610c3932888f441f1db05542bc2662b808aae
zenoix <41812358+zenoix@users.noreply.github.com>
2025-09-15 21:23:39 +1200

    fix(hyprland): update workspace swipe for new update

--- homeManagerModules/wm/hyprland.nix
+++ homeManagerModules/wm/hyprland.nix
@@ -125,10 +125,12 @@
           new_status = true;
         };
 
+        gesture = [
+          "3, horizontal, workspace"
+        ];
+
         gestures = {
-          workspace_swipe = true;
-          workspace_swipe_fingers = 3;
-          workspace_swipe_invert = true;
+          workspace_swipe_create_new = true;
           workspace_swipe_distance = 200;
           workspace_swipe_forever = true;
         };
