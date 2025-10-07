459d0de2a2b74a2bba4fbb80ae6c957faf492b9d
zenoix <41812358+zenoix@users.noreply.github.com>
2025-10-07 18:31:22 +1300

    chore(flake): update flake.lock

--- homeManagerModules/terminal-programs/nixvim/plugins/neogen.nix
+++ homeManagerModules/terminal-programs/nixvim/plugins/neogen.nix
@@ -1,26 +1,28 @@
 {
   programs.nixvim.plugins.neogen = {
     enable = true;
-    enablePlaceholders = true;
     keymapsSilent = true;
-    placeholderHighlight = "DiagnosticHint";
-    snippetEngine = "luasnip";
     keymaps = {
       generate = "<leader>d";
     };
 
-    placeholdersText = {
-      args = "[TODO:args]";
-      attribute = "[TODO:attribute]";
-      class = "[TODO:class]";
-      description = "[TODO:description]";
-      kwargs = "[TODO:kwargs]";
-      parameter = "[TODO:parameter]";
-      return = "[TODO:return]";
-      throw = "[TODO:throw]";
-      tparam = "[TODO:tparam]";
-      type = "[TODO:type]";
-      varargs = "[TODO:varargs]";
+    settings = {
+      enable_placeholders = true;
+      snippet_engine = "luasnip";
+      placeholder_hl = "DiagnosticHint";
+      placeholders_text = {
+        args = "[TODO:args]";
+        attribute = "[TODO:attribute]";
+        class = "[TODO:class]";
+        description = "[TODO:description]";
+        kwargs = "[TODO:kwargs]";
+        parameter = "[TODO:parameter]";
+        return = "[TODO:return]";
+        throw = "[TODO:throw]";
+        tparam = "[TODO:tparam]";
+        type = "[TODO:type]";
+        varargs = "[TODO:varargs]";
+      };
     };
   };
 }
